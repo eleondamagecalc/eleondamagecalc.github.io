b38e0bc6241c6b23766274c28ddd5f52cfbb5c60
eleondamagecalc <neoblackwood8@gmail.com>
2026-02-05 21:34:38 -0500

    Add files via upload

---
+++ src/builtInSets.js
@@ -0,0 +1,26 @@
+var builtInSets = [
+	{
+	    name: "Cotail",
+	    setName: "test",
+	    evs: {
+	        hp: 100,
+	        attack: 100,
+	        defense: 100,
+	        attackR: 100,
+	        defenseR: 100,
+	        speed: 100
+	    },
+
+	    moves: {
+	        move1: "Last Resort",
+	        move2: "Last Resort",
+	        move3: "Last Resort",
+	        move4: "Last Resort"
+	    },
+	    posNature: "strong",
+	    ability: "Mastery",
+	    item: "Power Glove",
+	    level: 50,
+	    builtIn: true
+	},
+	];
